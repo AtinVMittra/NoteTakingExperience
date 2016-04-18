1b376a454e9faf872187c1d217c4de74d72ccbaf
AtinVMittra <avmittra@gmail.com>
2016-04-18 16:37:21 -0400

    Added files via upload

--- NoteTakingExperience.js
+++ NoteTakingExperience.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+	var users = [
+  		{username: 'Bob', fullname: 'Bob Russel'},
+  		{username: 'Kimmy', fullname: 'Kimmy Schmidt'},
+  		{username: 'Jimmy', fullname: 'Jimmy Fallon'},
+	];
+
+	 var NTDL = [
+  		{type: 'note'},
+  		{type: 'task'},
+  		{type: 'decision'},
+  		{type: 'learning'},
+	]; 
+
+	var workspaces = [
+		{space: 'sales'},
+		{space: 'marketing'},
+		{space: 'business development'},
+		{space: 'product'},
+	];
+
     $("button").mouseup(function(){
         $(this).css("background-color", "#FFFFFF");
 	});
@@ -22,6 +42,37 @@ $(document).ready(function(){
     $("#learning").click(function(){
         $("#first").val("/learning");
     }); 
+
+	$('#first').suggest('/', {
+  		data: NTDL,
+  		map: function(user) {
+    		return {
+      			value: user.type,
+      			text: user.type
+		    }
+  		}
+	});
+
+	$('#second').suggest('@', {
+  		data: users,
+  		map: function(user) {
+    		return {
+      			value: user.username,
+      			text: user.username
+		    }
+  		}
+	});
+
+
+	$('#third').suggest('#', {
+  		data: workspaces,
+  		map: function(user) {
+    		return {
+      			value: user.space,
+      			text: user.space
+		    }
+  		}
+	});
 
 });
 
